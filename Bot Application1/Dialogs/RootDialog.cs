24de764f6a4c3ad1f127640d17d1196145f5972a
Arve Hansen <arveh4@outlook.com>
2017-05-08 14:27:04 +0200

    Added BotApplication1. Give your name, and it says "Hi <name>".

---
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            PromptDialog.Text(context, NameEntered, "Hi there, what's your name?");
+
+            return Task.CompletedTask;
+
+            //var activity = await result as Activity;
+
+            //// calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
+
+            //// return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            //context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task NameEntered(IDialogContext context, IAwaitable<string> result)
+        {
+            await context.PostAsync($@"Hi {await result}!");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
