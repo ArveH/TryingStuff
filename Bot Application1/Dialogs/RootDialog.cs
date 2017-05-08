b8e0405f5c4a386957c58b429907c19ea26bafab
Arve Hansen <arveh4@outlook.com>
2017-05-08 14:44:56 +0200

    Hold state. When I start a new conversation, it remembers who I am.

--- Bot Application1/Dialogs/RootDialog.cs
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -17,24 +17,18 @@ namespace Bot_Application1.Dialogs
 
         private Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            PromptDialog.Text(context, NameEntered, "Hi there, what's your name?");
+            context.Call<UserProfile>(new EnsureProfileDialog(), ProfileEnsured);
 
             return Task.CompletedTask;
-
-            //var activity = await result as Activity;
-
-            //// calculate something for us to return
-            //int length = (activity.Text ?? string.Empty).Length;
-
-            //// return our reply to the user
-            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-            //context.Wait(MessageReceivedAsync);
         }
 
-        private async Task NameEntered(IDialogContext context, IAwaitable<string> result)
+        private async Task ProfileEnsured(IDialogContext context, IAwaitable<UserProfile> result)
         {
-            await context.PostAsync($@"Hi {await result}!");
+            var profile = await result;
+
+            context.UserData.SetValue(@"profile", profile);
+
+            await context.PostAsync($@"Hello {profile.Name}, I love {profile.Company}!");
 
             context.Wait(MessageReceivedAsync);
         }
---
+++ Bot Application1/Model/UserProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Bot_Application1
+{
+    [Serializable]
+    public class UserProfile
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+}
